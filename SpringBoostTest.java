37d9a1ce45b71452a50ebf8cf4d6a7607dbe1a9c
ZhekaLited <94718651+ZhekaLited@users.noreply.github.com>
2022-07-26 21:26:56 +0300

    Update SpringBoostTest.java

--- SpringBoostTest.java
+++ SpringBoostTest.java
@@ -56,7 +56,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
        break;
             default:
                 actions
-                        .andExpect(content().string(""));
+                        .andExpect(content().string("null"));
         }
 
     }
